4c3344c92550e179f55c61e60c6a0a9c435be551
Ashutosh Shriansh <ashutosh.shriansh@ibm.com>
2022-05-21 02:46:00 +0530

    Eighth commit

---
+++ functional-programming-with-java/src/programming/CubeOddNumbersLambda.java
@@ -0,0 +1,22 @@
+package programming;
+
+import java.util.List;
+
+public class CubeOddNumbersLambda {
+
+	public static void main(String[] args) {
+
+		printSquaresOfOddNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+
+	}
+
+	private static void printSquaresOfOddNumbersInListFunctional(List<Integer> numbers) {
+
+		numbers.stream().
+		filter(number -> number % 2 == 0)
+		.map(number -> number * number * number)
+		.forEach(System.out::println);
+
+	}
+
+}

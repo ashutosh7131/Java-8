230bf28c4d67983e413943b7f0ee2ab35291c831
Ashutosh Shriansh <ashutosh.shriansh@ibm.com>
2022-05-27 12:13:50 +0530

    commit on 27-05-2022

--- functional-programming-with-java/src/programming/Employee.java
+++ functional-programming-with-java/src/programming/Employee.java
@@ -1,14 +1,12 @@
 package programming;
 
 public class Employee {
-	
+
 	public static void main(String[] args) {
-		
+
 		Employee e1 = new Employee();
 		System.out.println(e1);
-		
-	}
-	
-	
-}
 
+	}
+
+}
